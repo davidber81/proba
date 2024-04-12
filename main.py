947f40a7de7d9a58178965087bb8af1c152b7459
user <davidber81@mail.ru>
2024-04-12 17:41:38 +0300

    Module

--- main.py
+++ main.py
@@ -1,54 +1,29 @@
+import unittest
+from Student import Student
 
-import requests
-response = requests.get(
-    'https://api.github.com/search/repositories',
-    params={'q': 'requests+language:python'},
-)
+class RunningTests(unittest.TestCase, Student):
 
-json_response = response.json()
-repository = json_response['items'][0]
-print(f'Repository name: {repository["name"]}')
-print(f'Repository description: {repository["description"]}')
+    def setUp(self):
+        self.distance = 0
 
-import pandas as pd
-df = pd.DataFrame([[1,'Bob', 'Builder'],
-                  [2,'Sally', 'Baker'],
-                  [3,'Scott', 'Candle Stick Maker']],
-columns=['id','name', 'occupation'])
-print(df)
-df = pd.read_csv('WHR_2019.csv')
+    def test_walk(self):
+        student = Student("John")
+        for _ in range(10):
+            student.walk()
+        self.assertEqual(student.distance, 500, "Дистанции не равны" + str(student.distance) + "!= 500")
 
-import numpy as np
+    def test_run(self):
+        student1 = Student("Max")
+        for _ in range(10):
+            student1.run()
+        self.assertGreater(student1.distance, 1000, "Дистанции не равны" + str(student1.distance) + "!=1000")
 
-c = np.array([1, 2, 3])
-print(c)
-d = np.array([[1, 2], [3, 4]])
-print(np.cos(d))
-a = np.ones((2, 3))
-print(a)
-b = np.full((3, 2), 2)
-print(b)
-print(np.matmul(a, b))
+    def test_sravnenie(self):
+        student = Student("John")
+        student1 = Student("Max")
+        if student1 > student:
+            self.assertLess(student1.distance, 1000, "должен преодолеть дистанцию больше, чем", student.distance, 500)
 
-import matplotlib.pyplot as plt
-labels = ['2017', '2018', '2019', '2020', '2021']
-android_users = [85, 80, 75, 69.2, 91]
-ios_users = [15.0, 20.0, 25.0, 30.8, 9.0]
-width = 0.35
-fig, ax = plt.subplots()
-ax.bar(labels, android_users, width, label='Android')
-ax.bar(labels, ios_users, width, bottom=android_users,
-      label='iOS')
-ax.set_ylabel('Соотношение, в %')
-ax.set_title('Распределение устройств на Android и iOS')
-ax.legend(loc='lower left', title='Устройства')
-plt.show()
 
-from PIL import Image, ImageDraw
-image = Image.open('image-pillow.png')
-image.show()
-
-img = Image.new('RGBA', (200, 200), 'white')
-idraw = ImageDraw.Draw(img)
-idraw.rectangle((10, 10, 100, 100), fill='blue')
-img.show()
+if __name__ == '__main__':
+    unittest.main()
