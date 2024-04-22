85521007c043bac702e794595a4ee44b84eab353
user <davidber81@mail.ru>
2024-04-22 10:10:39 +0300

    Module

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
-import logging
-import bs4
 import requests as rq
+import logging
+import requests.exceptions
 
-logging.basicConfig(level=logging.INFO)
-
-sites = ['https://www.youtube.com/', 'https://instagram.com', 'https://wikipedia.org', 'https://yahoo.com',         'https://yandex.ru', 'https://whatsapp.com', 'https://twitter.com', 'https://amazon.com', 'https://tiktok.com',         'https://www.ozon.ru']
-
-info_handler = logging.FileHandler("success_responses.log")
-info_handler.setLevel(logging.INFO)
-warning_handler = logging.FileHandler("bad_responses.log")
-warning_handler.setLevel(logging.WARNING)
-error_handler = logging.FileHandler("blocked_responses.log")
-error_handler.setLevel(logging.ERROR)
-
+formatting = '[%(levelname)s]: %(message)s'
+logging.basicConfig(level=logging.INFO, format=formatting)
 logger = logging.getLogger('RequestsLogger')
-logger.addHandler(info_handler)
-logger.addHandler(warning_handler)
-logger.addHandler(error_handler)
+
+sites = ['https://www.youtube.com/',
+         'https://instagram.com',
+         'https://wikipedia.org',
+         'https://yahoo.com',
+         'https://yandex.ru',
+         'https://whatsapp.com',
+         'https://twitter.com',
+         'https://amazon.com',
+         'https://tiktok.com',
+         'https://www.ozon.ru']
+
+
+success_log = 'success_response.log'
+bad_log = 'bad_responses.log'
+blocked_log = 'blocked_response.log'
+
+def write_to_file(file_name, message):
+    with open(file_name, 'a') as file:
+        file.write(message + '\n')
 
 for site in sites:
     try:
         response = rq.get(site, timeout=3)
-        soup = bs4.BeautifulSoup(response.text, 'lxml')
-        print(response)
-        if response.status_code == 200:
-            logger.info(f'INFO: {site}, "response -", {response.status_code}')
-        else:
-            logger.warning(f'WARNING: {site}, "response -", {response.status_code}')
-    except Exception as e:
-        logger.error(f'Error: {site}, "NO CONNECTION"')
 
-for handler in logger.handlers:
-    handler.close()
+        if response.status_code == 200:
+            logging.info(f'{site} response - {response.status_code}')
+            write_to_file(success_log, f'INFO {site} response - {response.status_code}')
+
+        elif response.status_code != 200:
+            logging.warning(f'{site} response - {response.status_code}')
+            write_to_file(bad_log, f'WARNING {site} response - {response.status_code}')
+
+    except requests.exceptions.RequestException as e:
+        logging.error(f'{site} No Connection')
+        write_to_file(blocked_log, f'ERROR {site} NO CONNECTION')
