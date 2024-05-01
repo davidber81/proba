6cfb2c24628e8435cff3b8f6372d13d5cf17db70
user <davidber81@mail.ru>
2024-05-01 11:15:36 +0300

    Module

--- Project1/data_download.py
+++ Project1/data_download.py
@@ -1,17 +1,23 @@
+import os
 import yfinance as yf
 from typing import Optional, Union
 import pandas as pd
 
-
-def fetch_stock_data(ticker: str, period: str, start: Optional[str] = None, end: Optional[str] = None) -> Union[
-    pd.DataFrame, None]:
-
+def fetch_stock_data(ticker: str, period: str, start: Optional[str] = None,
+                     end: Optional[str] = None) -> Union[pd.DataFrame, None]:
+    """
+    Получает данные об акциях для указанного тикера из Yahoo Finance.
+    Параметры:
+        ticker (str): Символ акции, для которой нужно получить данные.
+        period (str): Период данных.
+        start (str, optional): Начальная дата периода данных.
+        end (str, optional): Конечная дата периода данных.
+    """
     try:
+        stock = yf.Ticker(ticker)
         if start is not None:
-            stock = yf.Ticker(ticker)
             data = stock.history(period=period, start=start, end=end)
         else:
-            stock = yf.Ticker(ticker)
             data = stock.history(period=period)
         print(data)
         return data
@@ -19,8 +25,13 @@ def fetch_stock_data(ticker: str, period: str, start: Optional[str] = None, end:
         print(f"\nОшибка при получении данных для тикера {ticker}: {e}")
         return None
 
-
-def add_moving_average(data, window_size=5):
+def add_moving_average(data: pd.DataFrame, window_size: int = 5) -> Union[pd.DataFrame, None]:
+    """
+    Считает и добавляет в DataFrame колонку со скользящим средним, рассчитанным на основе цен закрытия.
+    Параметры:
+        data (pd.DataFrame): Данные о цене закрытия акций с Yahoo Finance.
+        window_size (int): Размер окна для вычисления скользящего среднего.
+    """
     try:
         data['Moving_Average'] = data['Close'].rolling(window=window_size).mean()
         return data
@@ -28,37 +39,102 @@ def add_moving_average(data, window_size=5):
         print(f"\nОшибка при добавлении скользящего среднего: {e}")
         return None
 
-
-def calculate_and_display_average_price(data, ticker):
+def calculate_and_display_average_price(data: pd.DataFrame, ticker: str) -> None:
+    """
+    Вычисляет и выводит в консоль среднюю цену закрытия акций за заданный период.
+    Параметры:
+        data (pd.DataFrame): Данные о цене закрытия акций.
+        ticker (str): Тикер акции.
+    """
     try:
         avr_price = data['Close'].mean()
         avr_price_rounded = round(avr_price, 6)
-
         start_date = data.index.min().strftime('%Y-%m-%d')
         end_date = data.index.max().strftime('%Y-%m-%d')
-
-        print(f'\nСредняя цена закрытия акции "{ticker}" за период <{start_date}...{end_date}> '
+        print(
+            f'\nСредняя цена закрытия акции "{ticker}" за период <{start_date}...{end_date}> '
             f'составила: {avr_price_rounded}')
+        return avr_price_rounded
     except Exception as e:
         print(f"\nОшибка при вычислении и отображении средней цены: {e}")
 
-
-def notify_if_strong_fluctuations(data, ticker, threshold=5):
+def notify_if_strong_fluctuations(data: pd.DataFrame, ticker: str, threshold=5) -> None:
+    """
+    Вычисляет максимальное и минимальное значения цены закрытия и сравнивает с заданным порогом.
+    Если разница превышает порог, выводит уведомление.
+    Параметры:
+        data (pd.DataFrame): Данные о цене закрытия акций.
+        ticker (str): Тикер акции.
+        threshold (int): Порог для определения сильных колебаний.
+    """
     try:
         max_closing_price = data['Close'].max()
         min_closing_price = data['Close'].min()
         fluctuation = ((max_closing_price - min_closing_price) / min_closing_price) * 100
         fluctuation_rounded = round(fluctuation, 1)
         if fluctuation_rounded > threshold:
-            print(f'За указанный период отмечается сильное колебание '
+            print(
+                f'За указанный период отмечается сильное колебание '
                 f'цены закрытия акций "{ticker}": {fluctuation_rounded}%')
+            return fluctuation_rounded
     except Exception as e:
         print(f"\nОшибка при определении сильных колебаний: {e}")
 
-
-def export_data_to_csv(data, filename):
+def export_data_to_csv(data: pd.DataFrame, filename: str) -> None:
+    """
+    Экспортирует данные в формате CSV.
+    Параметры:
+        data (pd.DataFrame): Данные для экспорта.
+        filename (str): Имя файла для сохранения данных.
+    """
     try:
+        if not isinstance(data, pd.DataFrame):
+            raise TypeError("Параметр 'data' должен быть объектом pd.DataFrame")
+        if not isinstance(filename, str):
+            raise TypeError("Параметр 'filename' должен быть строкой")
+        if os.path.exists(filename):
+            print(f"Файл {filename} уже существует. Перезаписать?")
         data.to_csv(filename)
         print(f"\nДанные по запросу сохранены в файл {filename}")
     except Exception as e:
         print(f"\nОшибка при экспорте данных в CSV: {e}")
+
+def calculate_rsi(data: pd.DataFrame, window_size=14):
+    """
+    Принимает DataFrame с данными о цене закрытия акций, вычисляет RSI (Relative Strength Index) и добавляет его в
+    DataFrame.
+    Параметры:
+        data (pd.DataFrame): Данные о цене закрытия акций.
+        window_size (int): Размер окна для вычисления RSI.
+    """
+    try:
+        delta = data['Close'].diff()
+        gain = (delta.where(delta > 0, 0)).rolling(window=window_size).mean()
+        loss = (-delta.where(delta < 0, 0)).rolling(window=window_size).mean()
+        rs = gain / loss
+        rsi = 100 - (100 / (1 + rs))
+        data['RSI'] = rsi
+        return data
+    except Exception as e:
+        print(f"\nОшибка при расчёте RSI: {e}")
+        return None
+
+def calculate_macd(data: pd.DataFrame, short_window=12, long_window=26, signal_window=9):
+    """
+    Принимает DataFrame с данными о цене закрытия акций, вычисляет MACD (Moving Average Convergence Divergence) и
+    линию сигнала.
+    Параметры:
+        data (pd.DataFrame): Данные о цене закрытия акций.
+        short_window (int): Короткое окно для вычисления EMA.
+        long_window (int): Длинное окно для вычисления EMA.
+        signal_window (int): Окно для вычисления линии сигнала.
+    """
+    try:
+        short_ema = data['Close'].ewm(span=short_window, adjust=False).mean()
+        long_ema = data['Close'].ewm(span=long_window, adjust=False).mean()
+        data['MACD'] = short_ema - long_ema
+        data['Signal_Line'] = data['MACD'].ewm(span=signal_window, adjust=False).mean()
+        return data
+    except Exception as e:
+        print(f"\nОшибка при расчёте MACD: {e}")
+        return None
--- Project1/data_plotting.py
+++ Project1/data_plotting.py
@@ -1,16 +1,50 @@
 import os
 import matplotlib.pyplot as plt
 
-
-def create_and_save_plot(data, ticker, period, start_date, end_date, filename=None):
+def create_and_save_plot(data, ticker, period, start_date, end_date, filename = None):
+    """
+    Создает и сохраняет интерактивный график на основе данных о биржевой акции.
+    Параметры:
+        data: Данные о биржевой акции в формате DataFrame.
+        ticker: Тикер акции.
+        period: Период для данных.
+        start_date: Дата начала анализа.
+        end_date: Дата окончания анализа.
+        filename: Имя файла для сохранения графика. По умолчанию None.
+    """
     try:
 
-        fig, axs = plt.subplots(3, 1, figsize=(18, 12))
+        fig, axs = plt.subplots(3, 1, figsize=(18, 25))
 
+        # График цены закрытия, скользящего среднего и стандартного отклонения
         axs[0].plot(data.index, data['Close'], label='Цена закрытия', color='blue')
         axs[0].plot(data.index, data['Moving_Average'], label='Скользящее среднее', color='orange')
+        fill_label = 'Стандартное отклонение'
+        axs[0].fill_between(data.index, data['Close'] - data['Close'], color='lightblue',
+                            alpha=0.5, label=fill_label)
         axs[0].set_ylabel('Цена')
-        axs[0].set_title(f"Цены акций {ticker} и скользящее среднее")
+        axs[0].set_title(f"\nЦены акций {ticker}, скользящее среднее и стандартное отклонение\n", fontweight='bold',
+                         fontsize=16)
+        axs[0].legend()
+
+        # График RSI
+        axs[1].plot(data.index, data['RSI'], label='RSI', color='purple')
+        axs[1].axhline(70, color='r', linestyle='--')
+        axs[1].axhline(30, color='g', linestyle='--')
+        axs[1].set_ylabel('RSI')
+        axs[1].set_title(f"\nОтносительный индекс силы {ticker} (RSI)\n", fontweight='bold',
+                         fontsize=16)
+        axs[1].legend()
+
+        # График MACD
+        axs[2].plot(data.index, data['MACD'], label='MACD', color='blue')
+        axs[2].plot(data.index, data['Signal_Line'], label='Сигнальная линия', color='orange')
+        axs[2].set_ylabel('MACD')
+        axs[2].set_title(f"\nСхождение и расхождение скользящих средних {ticker} (MACD)\n", fontweight='bold',
+                         fontsize=16)
+        axs[2].legend()
+
+        fig.suptitle(f"Анализ акций {ticker}", fontsize=20, fontweight='bold')  # Общий заголовок
 
         plt.xlabel("Дата")
 
@@ -20,6 +54,7 @@ def create_and_save_plot(data, ticker, period, start_date, end_date, filename=No
         plt.savefig(filename)
         print(f"Графики сохранены в {filename}")
 
+        # Проверка наличия файла
         if os.path.exists(filename):
             print(f"Файл {filename} был успешно создан.")
         else:
