85453aa780326635672e922a036d7df7bc77a6bd
user <davidber81@mail.ru>
2024-05-13 19:23:02 +0300

    Module

--- Project1/test_data_donwnload.py
+++ Project1/test_data_donwnload.py
@@ -1,8 +1,9 @@
 import unittest
 import pandas as pd
 from unittest.mock import patch
-from data_download import fetch_stock_data, add_moving_average, calculate_and_display_average_price, \
-    notify_if_strong_fluctuations
+from data_download import fetch_stock_data, add_moving_average, calculate_and_display_average_price, calculate_rsi, \
+    calculate_macd, notify_if_strong_fluctuations, export_data_to_csv, calculate_standard_deviation
+import tempfile
 
 
 class TestDataDownload(unittest.TestCase):
@@ -29,6 +30,32 @@ class TestDataDownload(unittest.TestCase):
         data = pd.DataFrame({'Close': [10, 20, 30, 40, 50]})
         notify_if_strong_fluctuations(data, 'AAPL', 5)
         mocked_print.assert_called()
+
+    @patch('builtins.print')
+    def test_export_data_to_csv(self, mocked_print):
+        data = pd.DataFrame({'Close': [10, 20, 30, 40, 50]})
+        with tempfile.NamedTemporaryFile() as tf:
+            export_data_to_csv(data, tf.name)
+            mocked_print.assert_called_with('\nДанные по запросу сохранены в файл ' + tf.name)
+            result_data = pd.read_csv(tf.name)
+            pd.testing.assert_frame_equal(data, result_data.iloc[:, 1:],
+                                          check_dtype=False)  # Adjusting columns due to index
+
+    def test_calculate_rsi(self):
+        data = pd.DataFrame({'Close': [10, 20, 15, 25, 30]})
+        data_with_rsi = calculate_rsi(data)
+        self.assertIn('RSI', data_with_rsi.columns)
+
+    def test_calculate_macd(self):
+        data = pd.DataFrame({'Close': [10, 20, 15, 25, 30]})
+        data_with_macd = calculate_macd(data)
+        self.assertIn('MACD', data_with_macd.columns)
+        self.assertIn('Signal_Line', data_with_macd.columns)
+
+    def test_calculate_standard_deviation(self):
+        data = pd.DataFrame({'Close': [10, 20, 15, 25, 30]})
+        std_deviation = calculate_standard_deviation(data)
+        self.assertIsNotNone(std_deviation)
 
 
 if __name__ == '__main__':
---
+++ Project2/main.py
@@ -0,0 +1,149 @@
+import tkinter as tk
+from tkinter import colorchooser, filedialog, messagebox
+from PIL import Image, ImageDraw
+
+class DrawingApp:
+    def __init__(self, root):
+        """
+        Инициализирует приложение для рисования.
+        Параметры:
+            root (tk.Tk): Корневой виджет Tkinter.
+        """
+        self.brush_size_scale = None
+        self.root = root
+        self.root.title("Рисовалка с сохранением в PNG")
+
+        self.image = Image.new("RGB", (600, 400), "white")
+        self.draw = ImageDraw.Draw(self.image)
+
+        self.canvas = tk.Canvas(root, width=600, height=400, bg='white')
+        self.canvas.pack()
+
+        self.brush_size = tk.IntVar()
+        self.brush_size.set(1)
+
+        self.setup_ui()
+
+        self.last_x, self.last_y = None, None
+        self.pen_color = 'black'
+        self.last_color = 'black'
+
+        self.canvas.bind('<B1-Motion>', self.paint)
+        self.canvas.bind('<ButtonRelease-1>', self.reset)
+
+    def setup_ui(self):
+        """
+        Настраивает интерфейс приложения.
+        """
+        control_frame = tk.Frame(self.root)
+        control_frame.pack(fill=tk.X)
+
+        clear_button = tk.Button(control_frame, text="Очистить", command=self.clear_canvas)
+        clear_button.pack(side=tk.LEFT)
+
+        color_button = tk.Button(control_frame, text="Выбрать цвет", command=self.choose_color)
+        color_button.pack(side=tk.LEFT)
+
+        brush_button = tk.Button(control_frame, text="Кисть", command=self.use_brush)
+        brush_button.pack(side=tk.LEFT)
+
+        save_button = tk.Button(control_frame, text="Сохранить", command=self.save_image)
+        save_button.pack(side=tk.LEFT)
+
+        brush_size_label = tk.Label(control_frame, text="Размер кисти:")
+        brush_size_label.pack(side=tk.LEFT, padx=(10, 2))
+
+        brush_size_frame = tk.Frame(control_frame)
+        brush_size_frame.pack(side=tk.LEFT)
+
+        sizes = [1, 3, 5, 10]
+        self.create_brush_size_menu(brush_size_frame, sizes)
+
+    def create_brush_size_menu(self, parent, sizes):
+        """
+        Создает меню выбора размера кисти.
+        Параметры:
+            parent (tk.Frame): Родительский фрейм для размещения меню.
+            sizes (list): Список размеров кисти.
+        """
+        size_menu = tk.OptionMenu(parent, self.brush_size, *sizes, command=self.update_brush_size)
+        size_menu.pack(side=tk.LEFT)
+
+        tk.Label(parent, text="   ").pack(side=tk.LEFT)
+
+        self.brush_size_scale = tk.Scale(parent, from_=1, to=10, orient=tk.HORIZONTAL,
+                                         variable=self.brush_size, length=100,
+                                         command=self.update_brush_size)
+        self.brush_size_scale.pack(side=tk.LEFT)
+
+    def update_brush_size(self, value):
+        """
+        Обновляет размер кисти.
+        Параметры:
+            value: Новое значение размера кисти.
+        """
+        self.brush_size.set(value)
+
+    def paint(self, event):
+        """
+        Рисует на холсте при движении мыши.
+        Параметры:
+            event: Событие мыши.
+        """
+        if self.last_x and self.last_y:
+            self.canvas.create_line(self.last_x, self.last_y, event.x, event.y,
+                                    width=self.brush_size.get(), fill=self.pen_color,
+                                    capstyle=tk.ROUND, smooth=tk.TRUE)
+            self.draw.line([self.last_x, self.last_y, event.x, event.y], fill=self.pen_color,
+                           width=self.brush_size.get())
+
+        self.last_x = event.x
+        self.last_y = event.y
+
+    def reset(self, event):
+        """
+        Сбрасывает последние координаты.
+        """
+        self.last_x, self.last_y = None, None
+
+    def clear_canvas(self):
+        """
+        Очищает холст.
+        """
+        self.canvas.delete("all")
+        self.image = Image.new("RGB", (600, 400), "white")
+        self.draw = ImageDraw.Draw(self.image)
+
+    def choose_color(self):
+        """
+        Позволяет выбрать цвет кисти.
+        """
+        color = colorchooser.askcolor(color=self.last_color)
+        if color[1]:
+            self.pen_color = color[1]
+            self.last_color = self.pen_color
+
+    def use_brush(self):
+        """
+        Переключает режим на кисть.
+        """
+        self.pen_color = self.last_color
+        self.last_color = self.pen_color
+
+    def save_image(self):
+        """
+        Сохраняет изображение в формате PNG.
+        """
+        file_path = filedialog.asksaveasfilename(filetypes=[('PNG files', '*.png')])
+        if file_path:
+            if not file_path.endswith('.png'):
+                file_path += '.png'
+            self.image.save(file_path)
+            messagebox.showinfo("Информация", f"Изображение успешно сохранено {file_path}")
+def main():
+    root = tk.Tk()
+    DrawingApp(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
